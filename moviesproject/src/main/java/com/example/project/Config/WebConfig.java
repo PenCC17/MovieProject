4f6a8e8eb370a06cfab9aa43930a757ae4bf9c2b
Isaac Pena <isaac.pena.pal@gmail.com>
2025-11-05 13:27:44 -0600

    allowed origin

--- moviesproject/src/main/java/com/example/project/Config/WebConfig.java
+++ moviesproject/src/main/java/com/example/project/Config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://trng2309-10.s3-website.us-east-2.amazonaws.com/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowCredentials(true);
                         
